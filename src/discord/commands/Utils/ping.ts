a89655f8ce3b650e4750d9cf0a1febf2195a6a76
NatanaelMiranda <natanael.santos.play@gmail.com>
2023-09-26 01:05:07 -0300

    feat: add command ping

---
+++ src/discord/commands/Utils/ping.ts
@@ -0,0 +1,26 @@
+import { Command } from "@/discord/base";
+import { settings } from "@/settings";
+import { hexToRgb } from "@magicyan/core";
+import { ApplicationCommandType, ColorResolvable, EmbedBuilder } from "discord.js";
+
+new Command({
+    name: "ping",
+    description: "veja o ping do bot!",
+    dmPermission: false,
+    type: ApplicationCommandType.ChatInput,
+    async run(interaction) {
+        const {user, client} = interaction;
+
+        let name = user.username;
+        let ping = client.ws.ping
+
+        let embed1 = new EmbedBuilder({
+            title: "Pong",
+            color: hexToRgb(settings.colors.theme.magic),
+            description: `Olá ${name}, o ping do bot é: \`${ping}ms\``,
+            timestamp: "",
+        })
+        
+        await interaction.reply({ephemeral: true, embeds: [embed1] })
+    },
+})
