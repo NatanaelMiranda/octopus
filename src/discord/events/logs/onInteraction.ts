88370fffeb623aed1b9bcee5dd57c0f6f4d40e0d
NatanaelMiranda <natanael.santos.play@gmail.com>
2023-10-08 02:44:30 -0300

    feat: add event log onInteraction

---
+++ src/discord/events/logs/onInteraction.ts
@@ -0,0 +1,35 @@
+import { Event } from "@/discord/base";
+import { time } from "discord.js";
+
+const logsChannelId = "1160437389324455968";
+
+new Event({
+  name: "interactionCreate",
+  async run(interaction) {
+    if (!interaction.inCachedGuild()) return;
+
+    if (interaction.isCommand()) {
+      const logsChannel = interaction.guild.channels.cache.get(logsChannelId);
+      if (!logsChannel?.isTextBased()) return;
+
+      const { channel, user, commandName, createdAt, commandType } =
+        interaction;
+      const emoji = ["⌨️", "👨‍🦱", "✉️"];
+      const text = [
+        "usou o comando",
+        "usou o contexto de usuário",
+        "usou o contexto de mensagem",
+      ];
+
+      let content = `${emoji[commandType - 1]} ${time(createdAt, "R")}`;
+      content += `**@${user.username}** `;
+      content += `__${text[commandType - 1]}__ `;
+      content += `\`${commandName}\` `;
+      if (channel) content += `em ${channel.url}`;
+
+      logsChannel.send({ content });
+
+      return;
+    }
+  },
+});
