43fc190a1e1cfe02615b0fc32c82f6f9c81b76b9
NatanaelMiranda <natanael.santos.play@gmail.com>
2023-10-11 18:03:17 -0300

    feat: add db firebese

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
-import { existsSync } from "node:fs";
-import { resolve } from "node:path";
+import { existsSync, readFileSync } from "node:fs";
+import { resolve, basename } from "node:path";
 import { Signale } from "signale";
 import settings from "./settings.json";
 import "./constants";
+import ck from "chalk";
+import { ServiceAccount } from "firebase-admin";
 
 const developmentEnvPath = resolve(__rootname, ".env.development");
 
@@ -23,4 +25,20 @@ const log = new Signale({ types: {
     successComponent: { badge: "√", color: "cyan", label: "Component" }
 }});
 
-export { log, processEnv, settings };
+const firebaseAccountPath = dev 
+? resolve(__rootname, "firebase.development.json")
+: resolve(__rootname, "firebase.json");
+
+if (!existsSync(firebaseAccountPath)){
+    const filename = ck.yellow(`"${basename(firebaseAccountPath)}"`);
+    const directory = ck.cyan(resolve(firebaseAccountPath, ".."));
+    const text = ck.red(`The ${filename} file was not found in ${directory}`);
+    console.error(text);
+    process.exit(0);
+}
+
+const firebaseAccount: ServiceAccount = JSON.parse(
+    readFileSync(firebaseAccountPath, {encoding: "utf-8"})
+);
+
+export { log, processEnv, settings, firebaseAccount };
