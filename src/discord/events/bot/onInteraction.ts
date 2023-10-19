2f803034417077baf5201ffc8a6883e2300f3b84
NatanaelMiranda <natanael.santos.play@gmail.com>
2023-10-19 01:20:11 -0300

    feat: Add command log activation check

--- src/discord/events/bot/onInteraction.ts
+++ src/discord/events/bot/onInteraction.ts
@@ -6,32 +6,39 @@ new Event({
   name: "interactionCreate",
   async run(interaction) {
     if (!interaction.inCachedGuild()) return;
-    const {guild} = interaction;
+    const { guild } = interaction;
 
-    const idData = await db.get(db.guilds, guild.id);
+    const action = await db.get(db.guilds, guild.id);
 
-    if (interaction.isCommand()) {
-      const logsChannel = interaction.guild.channels.cache.get(idData?.logs?.channel || "");
-      if (!logsChannel?.isTextBased()) return;
+    const actionLog = action?.logs?.commandLog == "true" ? "true" : "false";
 
-      const { channel, user, commandName, createdAt, commandType } =
-        interaction;
-      const emoji = ["⌨️", "👨‍🦱", "✉️"];
-      const text = [
-        "usou o comando",
-        "usou o contexto de usuário",
-        "usou o contexto de mensagem",
-      ];
+    if (actionLog == "false") return;
 
-      let content = `${emoji[commandType - 1]} ${time(createdAt, "R")}`;
-      content += `**@${user.username}** `;
-      content += `__${text[commandType - 1]}__ `;
-      content += `\`${commandName}\` `;
-      if (channel) content += `em ${channel.url}`;
+      if (interaction.isCommand()) {
 
-      logsChannel.send({ content });
+        const logsChannel = interaction.guild.channels.cache.get(
+          action?.logs?.channel || ""
+        );
+        if (!logsChannel?.isTextBased()) return;
 
-      return;
-    }
+        const { channel, user, commandName, createdAt, commandType } =
+          interaction;
+        const emoji = ["⌨️", "👨‍🦱", "✉️"];
+        const text = [
+          "usou o comando",
+          "usou o contexto de usuário",
+          "usou o contexto de mensagem",
+        ];
+
+        let content = `${emoji[commandType - 1]} ${time(createdAt, "R")}`;
+        content += `**@${user.username}** `;
+        content += `__${text[commandType - 1]}__ `;
+        content += `\`${commandName}\` `;
+        if (channel) content += `em ${channel.url}`;
+
+        logsChannel.send({ content });
+
+        return;
+      }    
   },
 });
