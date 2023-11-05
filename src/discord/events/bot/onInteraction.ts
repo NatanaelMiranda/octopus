f790101159b78358485cc974b14d3331dd372033
NatanaelMiranda <natanael.santos.play@gmail.com>
2023-11-04 22:26:38 -0300

    fix: removendo command log

--- src/discord/events/bot/onInteraction.ts
+++
@@ -1,9 +0,0 @@
-import { Event } from "@/discord/base";
-import { LogCommand } from "@/functions";
-
-new Event({
-  name: "interactionCreate",
-  async run(interaction) {
-  LogCommand(interaction);  
-  },
-});
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,7 @@
 export * from "./systems/logs";
 export * from "./systems/register";
 export * from "./systems/global";
-export * from "./systems/command";
+
 export * from "./data/minecraft";
+
 export * from "./discord/reply";
